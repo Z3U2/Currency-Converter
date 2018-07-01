121cc29308715f7a5eda4824e562cd6eedd8bb80
z3u2 <ilyassnasr@gmail.com>
2018-07-01 01:27:14 +0000

    Adds slash to cash URLs

--- docs/sw.js
+++ docs/sw.js
@@ -6,12 +6,12 @@ self.addEventListener('install', function (event) {
         caches.open(staticCacheName).then(function (cache) {
             return cache.addAll([
                 '/',
-                'es6/app.js',
-                'css/one-page-wonder.css',
-                'vendor/bootstrap/css/bootstrap.min.css',
-                'vendor/bootstrap/js/bootstrap.bundle.min.js',
-                'vendor/jquery/jquery.min.js',
-                'vendor/idb/idb.js',
+                '/es6/app.js',
+                '/css/one-page-wonder.css',
+                '/vendor/bootstrap/css/bootstrap.min.css',
+                '/vendor/bootstrap/js/bootstrap.bundle.min.js',
+                '/vendor/jquery/jquery.min.js',
+                '/vendor/idb/idb.js',
                 'https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900',
                 'https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i',
                 'https://free.currencyconverterapi.com/api/v5/currencies'
